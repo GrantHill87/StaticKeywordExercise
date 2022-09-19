3731e94992a41c5d608e67d559900b7d74c65c1d
Grant Hill <granthill@live.com>
2022-09-18 23:33:02 -0400

    first commit.

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Static Keyword Exercise.");
+            Console.WriteLine();
+            Console.WriteLine("Utilizing static methods created within specific classes, we will call for those methods to be used here in the main script.");
+            Console.WriteLine();
+            var celsius = TempConverter.FahrenheitToCelsius(68.00);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20.00);
+            Console.WriteLine($"The Celsius based temperature is currently standing at {celsius} degrees centigrade.");
+            Console.WriteLine();
+            Console.WriteLine($"The Fahrenheit based temperature, with the same input, yields {fahrenheit} degrees fahrenheit.");
+        }
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            var result = (Fahrenheit - 32) / 1.8;
+            return result;
+        }
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
